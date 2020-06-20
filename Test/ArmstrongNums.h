0a03846fb267f08dca5a509734aa7000424ee3a3
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2020-06-20 14:16:14 +0300

    added comments
    documented functions

--- Test/ArmstrongNums.h
+++ Test/ArmstrongNums.h
@@ -1,7 +1,11 @@
-
-
 #include <stdio.h>
 #include <math.h>
+
+/*
+ Функция для подсчёта количества знаков в числе
+ Ввод: целое число
+ Вывод : количество знаков в виде целочисленной переменной
+*/
 
 int Number_of_Digits(int num){
     int digits = 0;
@@ -12,12 +16,24 @@ int Number_of_Digits(int num){
     return digits;
 }
 
+/*
+ Функция для разделения числа на массив цифр
+ Ввод: целое число, количество цифр в числе, массив для хранения цифр
+ Вывод : -
+*/
+
 void ToArray(int num,int length,int arr[]){
     for(int i = 0;i<length;i++){
         arr[i] = num%10;
         num/= 10;
     }
 }
+
+/*
+ Функция для определения является ли целочисленная переменная числом Армстронга
+ Ввод: целое число, количество цифр в числе, массив цифр
+ Вывод : переменная булевого типа
+*/
 
 bool Is_Armstrong_Num(int num,int length,int arr[]){
     int temp = 0;
@@ -27,6 +43,11 @@ bool Is_Armstrong_Num(int num,int length,int arr[]){
     return num==temp;
 }
 
+/*
+ Функция для вывода чисел Армстронга в указанном диапазоне
+ Ввод: два целых числа, первое отображает начало диапазона, а второе конец
+ Вывод : -
+*/
 
 void Print_Armstrong_Nums(int start,int end){
     for(int i = start;i<=end;i++){
